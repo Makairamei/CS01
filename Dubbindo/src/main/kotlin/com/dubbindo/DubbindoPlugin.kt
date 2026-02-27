8b2d85352762a2b9e0807e823934697b2c6eb2e6
CodeSanzz <mlus01089@gmail.com>
2026-02-27 13:46:30 +0700

    Add Dubbindo provider

---
+++ Dubbindo/src/main/kotlin/com/dubbindo/DubbindoPlugin.kt
@@ -0,0 +1,13 @@
+
+package com.dubbindo
+
+import com.byayzen.Dubbindo
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DubbindoPlugin: Plugin() {
+    override fun load() {
+        registerMainAPI(Dubbindo())
+    }
+}
