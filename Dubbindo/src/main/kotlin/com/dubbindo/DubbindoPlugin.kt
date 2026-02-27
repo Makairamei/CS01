2a772c2b15dc547a4e065f314f64cd223359ae17
HXznxinhanba10fa9v-coDekxz <janxinhanbafav@gmail.com>
2026-02-27 13:59:27 +0700

    Update DubbindoPlugin.kt

--- Dubbindo/src/main/kotlin/com/dubbindo/DubbindoPlugin.kt
+++ Dubbindo/src/main/kotlin/com/dubbindo/DubbindoPlugin.kt
@@ -1,12 +1,11 @@
-
 package com.dubbindo
 
-import com.byayzen.Dubbindo
+import com.dubbindo.Dubbindo
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class DubbindoPlugin: Plugin() {
+class DubbindoPlugin : Plugin() {
     override fun load() {
         registerMainAPI(Dubbindo())
     }
