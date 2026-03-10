be123661e1485787f2a08c97bb3ffaa6bea94413
CodeSanzz <mlus01089@gmail.com>
2026-03-11 04:36:35 +0700

    fix gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,93 +5,96 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
+repositories {
+google()
+mavenCentral()
+maven("https://jitpack.io")
+}
 
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.13.2")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
-    }
+dependencies {  
+    classpath("com.android.tools.build:gradle:8.13.2")  
+    classpath("com.github.recloudstream:gradle:master-SNAPSHOT")  
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")  
+}  
+
 }
 
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
+repositories {
+google()
+mavenCentral()
+maven("https://jitpack.io")
+}
 }
 
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
-    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
 fun Project.android(configuration: BaseExtension.() -> Unit) =
-    extensions.getByName<BaseExtension>("android").configuration()
+extensions.getByName<BaseExtension>("android").configuration()
 
 subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
+apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-    cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY")
-            ?: "https://github.com/phisher98/cloudstream-extensions-phisher")
-        authors = listOf("Phisher98")
-    }
+cloudstream {  
+    setRepo(System.getenv("GITHUB_REPOSITORY")  
+        ?: "https://github.com/phisher98/cloudstream-extensions-phisher")  
+    authors = listOf("Phisher98")  
+}  
 
-    android {
-        namespace = "com.phisher98"
+android {  
+    namespace = "com.phisher98"  
 
-        defaultConfig {
-            minSdk = 21
-            targetSdk = 35
-            compileSdk = 35
-        }
+    compileSdkVersion(35)  
 
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
+    defaultConfig {  
+        minSdk = 21  
+        targetSdk = 35  
+    }  
 
-        tasks.withType<KotlinJvmCompile> {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8)
-                freeCompilerArgs.addAll(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions",
-                    "-Xskip-metadata-version-check"
-                )
-            }
-        }
-    }
+    compileOptions {  
+        sourceCompatibility = JavaVersion.VERSION_1_8  
+        targetCompatibility = JavaVersion.VERSION_1_8  
+    }  
 
-    dependencies {
-        val implementation by configurations
-        val cloudstream by configurations
+    tasks.withType<KotlinJvmCompile> {  
+        compilerOptions {  
+            jvmTarget.set(JvmTarget.JVM_1_8)  
+            freeCompilerArgs.addAll(  
+                "-Xno-call-assertions",  
+                "-Xno-param-assertions",  
+                "-Xno-receiver-assertions",  
+                "-Xskip-metadata-version-check"  
+            )  
+        }  
+    }  
+}  
 
-        cloudstream("com.lagradost:cloudstream3:pre-release")
+dependencies {  
+    val implementation by configurations  
+    val cloudstream by configurations  
 
-        implementation(kotlin("stdlib", "2.3.0"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.16")
-        implementation("org.jsoup:jsoup:1.22.1")
-        implementation("androidx.annotation:annotation:1.9.1")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-        implementation("org.mozilla:rhino:1.9.0")
-        implementation("me.xdrop:fuzzywuzzy:1.4.0")
-        implementation("com.google.code.gson:gson:2.13.2")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
-        implementation("com.github.vidstige:jadb:v1.2.1")
-        implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
-    }
+    cloudstream("com.lagradost:cloudstream3:pre-release")  
+
+    implementation(kotlin("stdlib", "2.3.0"))  
+    implementation("com.github.Blatzar:NiceHttp:0.4.16")  
+    implementation("org.jsoup:jsoup:1.22.1")  
+    implementation("androidx.annotation:annotation:1.9.1")  
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")  
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")  
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")  
+    implementation("org.mozilla:rhino:1.9.0")  
+    implementation("me.xdrop:fuzzywuzzy:1.4.0")  
+    implementation("com.google.code.gson:gson:2.13.2")  
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")  
+    implementation("com.github.vidstige:jadb:v1.2.1")  
+    implementation("org.bouncycastle:bcpkix-jdk15on:1.70")  
+}  
+
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+delete(rootProject.layout.buildDirectory)
 }
